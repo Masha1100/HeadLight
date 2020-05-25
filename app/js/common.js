044682babf52d5be3ac5b53780d5a638a917499a
Mikhail <35425710+GrefferM@users.noreply.github.com>
2020-05-25 11:56:07 +0300

    This commit adds a nav block.

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,17 @@ document.addEventListener("DOMContentLoaded", initJs)
 function initJs() {
 	const d = document;
 	const w = window;
+
+	const mn_slider = d.querySelector('.mn-slider__items');
+
+	if (mn_slider) {
+		$(mn_slider).slick({
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			dots: true
+		})
+	}
 }
 
 function autoHeight(el) {
