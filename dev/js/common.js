a6eb8af6cc738bf166a9888a02d87e095dc64b51
Mikhail <35425710+GrefferM@users.noreply.github.com>
2020-05-26 22:25:48 +0300

    his commit corrects the display about as well as the correction of common.sass styles.

--- dev/js/common.js
+++ dev/js/common.js
@@ -9,21 +9,44 @@ function initJs() {
 	const mn_slider = d.querySelector('.mn-slider__items');
 	const ml_slider = d.querySelector('.ml-slider__items');
 
-	if (ml_slider) {
-		$(ml_slider).slick({
-			arrows: false,
-			infinite: false,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-			dots: true
-		})
-	}
+	// Main Slider -> index.html
 	if (mn_slider) {
 		$(mn_slider).slick({
 			infinite: true,
 			slidesToShow: 1,
 			slidesToScroll: 1,
 			dots: true
+		})
+	}
+	// LEADERS FOR SALE -> index.html
+	if (ml_slider) {
+		$(ml_slider).slick({
+			arrows: false,
+			infinite: false,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			dots: true,
+			responsive: [
+				{
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 3
+				}
+				},
+				{
+					breakpoint: 769,
+					settings: {
+						slidesToShow: 2
+					}
+				},
+				{
+					breakpoint: 426,
+					settings: {
+						vertical: true,
+						slidesToShow: 1
+					}
+				}
+			]
 		})
 	}
 }
