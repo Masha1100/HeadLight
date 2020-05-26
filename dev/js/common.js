1ea14b1799a625e33edf93644b025b1a7dfdea41
Mikhail <35425710+GrefferM@users.noreply.github.com>
2020-05-26 12:14:16 +0300

    It commits the ml-slider block

--- dev/js/common.js
+++ dev/js/common.js
@@ -7,7 +7,16 @@ function initJs() {
 	const w = window;
 
 	const mn_slider = d.querySelector('.mn-slider__items');
+	const ml_slider = d.querySelector('.ml-slider__items');
 
+	if (ml_slider) {
+		$(ml_slider).slick({
+			infinite: true,
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			dots: true
+		})
+	}
 	if (mn_slider) {
 		$(mn_slider).slick({
 			infinite: true,
